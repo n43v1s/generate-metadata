3a5146a1d32ef89a87050573eabf7d4991ccaf60
agus.rokyanto <agus.rokyanto@ist.id>
2025-04-09 14:40:43 +0700

    Initialize Fatjar Reflection

---
+++ src/main/java/org/example/generatemetadata/experimental/GenerateFatJarMetadata.java
@@ -0,0 +1,7 @@
+package org.example.generatemetadata.experimental;
+
+public class GenerateFatJarMetadata {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
