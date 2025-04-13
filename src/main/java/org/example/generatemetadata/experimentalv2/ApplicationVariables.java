8a0ab70ca6ed0f2a5aa860f2810ff2230b1bae4b
agus.rokyanto <agus.rokyanto@ist.id>
2025-04-13 20:32:33 +0700

    Separate Variables, Enhance Path Validation, Logging Project Information, Separate Initialization

--- src/main/java/org/example/generatemetadata/experimentalv2/ApplicationVariables.java
+++ src/main/java/org/example/generatemetadata/experimentalv2/ApplicationVariables.java
@@ -1,33 +1,47 @@
 package org.example.generatemetadata.experimentalv2;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.example.generatemetadata.experimental.ConsoleColors.PURPLE;
+import static org.example.generatemetadata.experimental.ConsoleColors.RESET;
 
 public class ApplicationVariables {
     /**
      * Properties
      * */
-    public static String projectName = "credit-balance-history";
-    public static String excludedImportPrefix = "import id.co.xl."+ projectName +".";
+    public static String projectName = "recharge-history";
     public static String version = "1.0.0-Alpha";
     public static boolean includeSpring = false;
     public static String directorySlash = "\\";
     static int count = 0;
-    public static Path logoPath = Paths.get("C:\\Users\\agusilaban\\xl\\generate-metadata\\src\\main\\resources\\banner.txt");
+    public static String projectArtifactId;
+    public static String projectGroupId;
+    public static String excludedImportPrefix;
 
 
     /**
-     * Input
+     * Paths
      * */
     public static Path projectPath = Paths.get("C:\\Users\\agusilaban\\xl\\" + projectName);
     public static Path repositoryPath = Paths.get("C:\\Users\\agusilaban\\.m2\\repository\\");
-    public static Path dependenciesListTxtPath = Paths.get(projectPath + "\\dependencies.txt");
+    public static Path outputPath = Paths.get("C:\\Users\\agusilaban\\Downloads\\agus\\complete");
 
 
     /**
-     * Output
+     * File Name
      * */
-    public static Path outputPath = Paths.get("C:\\Users\\agusilaban\\Downloads\\agus\\experimental");
+    // Maven Dependency List
+    public static Path mavenDependenciesListTxtPath = projectPath.resolve("dependencies.txt");
+    public static Path projectPomPath = projectPath.resolve("pom.xml");
 
     // Reflect Config
     public static Path projectReflectionPath = outputPath.resolve("project-reflect-config.json");
@@ -45,9 +59,65 @@ public class ApplicationVariables {
 
 
     /**
-     * Final Output
+     * Output
      * */
-    public static Path reflectConfigFile = Paths.get("reflect-config.json");
-    public static Path proxyConfigFile = Paths.get("proxy-config.json");
+    public static Path reflectConfigFile = outputPath.resolve("reflect-config.json");
+    public static Path proxyConfigFile = outputPath.resolve("proxy-config.json");
+
+
+    public static void constructProjectGroupIdAndArtifactId () {
+        try {
+            Path pomPath = Paths.get(String.valueOf(projectPomPath));
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader(String.valueOf(pomPath)));
+            projectArtifactId = model.getArtifactId();
+            projectGroupId = model.getGroupId();
+            excludedImportPrefix = "import " + projectGroupId + "." + constructProjectMainDir().values().toArray()[0] +".";
+            System.out.println(PURPLE + "Artifact Id" + RESET + " -> " + projectArtifactId);
+            System.out.println(PURPLE + "Group Id" + RESET + " -> " + projectGroupId);
+            System.out.println(PURPLE + "Excluded Import Prefix" + RESET + " -> " + excludedImportPrefix + "\n");
+        } catch (Exception e) {
+            System.out.println("Error reading POM file: " + e.getMessage());
+        }
+    }
+    public static Map<String, String> constructProjectMainDir () {
+        Map<String, String> result = new HashMap<>();
+        try {
+            Path path1 = Paths.get(
+                    projectPath +
+                            "\\src\\main\\java\\" +
+                            projectGroupId.replace(".", directorySlash) +
+                            directorySlash +
+                            projectName
+            );
+            Path path2 = Paths.get(
+                    projectPath +
+                    "\\src\\main\\java\\" +
+                    projectGroupId.replace(".", directorySlash) +
+                    directorySlash +
+                    projectName.replace("-", "")
+                    );
+            Path path3 = Paths.get(
+                    projectPath +
+                            "\\src\\main\\java\\" +
+                            projectGroupId.replace(".", directorySlash) +
+                            directorySlash +
+                            projectName.replace("-", directorySlash)
+            );
+
+            if (Files.isDirectory(path1)) {
+                result.put(String.valueOf(path1), projectName);
+            } else if (Files.isDirectory(path2)) {
+                result.put(String.valueOf(path2), projectName.replace("-", ""));
+            } else if (Files.isDirectory(path3)) {
+                result.put(String.valueOf(path2), projectName.replace("-", "."));
+            } else {
+                result = null;
+            }
+        } catch (Exception e) {
+            System.out.println("Error constructing project main directory: " + e.getMessage());
+        }
+        return result;
+    }
 }
 
--- src/main/java/org/example/generatemetadata/experimentalv2/GenerateMetadata.java
+++ src/main/java/org/example/generatemetadata/experimentalv2/GenerateMetadata.java
@@ -1,18 +1,24 @@
 package org.example.generatemetadata.experimentalv2;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static org.example.generatemetadata.experimental.ConsoleColors.*;
 import static org.example.generatemetadata.experimentalv2.ApplicationVariables.*;
 
 public class GenerateMetadata {
     public static void main(String[] args) {
-        printLogo();
-        isAllPathsValid();
-        listAllDependencies();
-        buildFatJar();
+        initializeProject();
 
         scanProject();
         scanProjectImports();
@@ -25,16 +31,24 @@ public class GenerateMetadata {
         System.out.println("Finished Generating Metadata");
     }
 
-    public static void isAllPathsValid () {
-        System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting path validation");
+    // ToDo : Project initialization (DONE)
+    public static void initializeProject () {
+        // System.out.println(BLUE + "[RUNNING] \t " + RESET + "Initializing project");
         try {
+            printLogo();
+            constructProjectGroupIdAndArtifactId();
+            constructProjectMainDir();
 
+            isAllPathsValid();
+            listAllDependencies();
+            buildFatJar();
         } catch (Exception e){
-            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred during path validation: " + e.getMessage());
+            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred during project initialization: " + e.getMessage());
         }
-        System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Path validation complete\n");
+        // System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Project initialization complete\n");
     }
 
+    // ToDo : Scan project for reflections and proxies ()
     public static void scanProject () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting project scan");
         try {
@@ -45,6 +59,7 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Project scan finished\n");
     }
 
+    // ToDo : Scan project's imports for reflections and proxies ()
     public static void scanProjectImports () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting project imports scan");
         try {
@@ -55,6 +70,7 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Project imports scan finished\n");
     }
 
+    // ToDo : Scan project's POM for reflections and proxies ()
     public static void scanProjectPom () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting project POM scan");
         try {
@@ -65,6 +81,7 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Project pom POM finished\n");
     }
 
+    // ToDo : Scan project's dependencies for reflections and proxies ()
     public static void scanProjectDependencies () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting project dependencies scan");
         try {
@@ -75,6 +92,7 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Project dependencies scan finished\n");
     }
 
+    // ToDo : Scan project's fat jar for reflections and proxies ()
     public static void scanProjectFatJar () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting project fat jar scan");
         try {
@@ -85,26 +103,7 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Project fat jar scan finished\n");
     }
 
-    public static void listAllDependencies () {
-        System.out.println(BLUE + "[RUNNING] \t " + RESET + "Listing all project dependencies");
-        try {
-
-        } catch (Exception e){
-            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred while listing project dependencies: " + e.getMessage());
-        }
-        System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Finished listing all project dependencies\n");
-    }
-
-    public static void buildFatJar () {
-        System.out.println(BLUE + "[RUNNING] \t " + RESET + "Building project fat jar");
-        try {
-
-        } catch (Exception e){
-            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred while building project fat jar: " + e.getMessage());
-        }
-        System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Finished Building project fat jar\n");
-    }
-
+    // ToDo : Merge all output reflect configs ()
     public static void configureReflectConfig () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Configuring reflection metadata");
         try {
@@ -115,6 +114,7 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Finished configuring reflection metadata\n");
     }
 
+    // ToDo : Merge all output proxy configs ()
     public static void configureProxyConfig () {
         System.out.println(BLUE + "[RUNNING] \t " + RESET + "Configuring proxy metadata");
         try {
@@ -125,20 +125,220 @@ public class GenerateMetadata {
         System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Finished configuring proxy metadata\n");
     }
 
-    public static void writeReflectConfig () {}
-
-    public static void writeProxyConfig () {}
-
-    public static void printLogo() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(logoPath)))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(WHITE + line + RESET);
+    // ToDo : Write reflect config (DONE)
+    public static void writeReflectConfig (List<String> classList, Path path) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayNode reflection = objectMapper.createArrayNode();
+        try {
+            if (classList.isEmpty()) {
+                System.out.println(YELLOW + "[WARN] \t " + RESET + "No classes found to write to file");
+            } else {
+                for (String className : classList) {
+                    String fullClassName = className.replace("import", "").replace(";", "").trim();
+                    ObjectNode classNode = objectMapper.createObjectNode();
+                    classNode.put("name", fullClassName);
+                    classNode.put("allDeclaredFields", true);
+                    classNode.put("allDeclaredMethods", true);
+                    classNode.put("allDeclaredConstructors", true);
+                    reflection.add(classNode);
+                }
+                objectMapper.writeValue(new File(path.toString()), reflection);
+                System.out.println(BLUE + "[INFO] \t " + RESET + "Wrote " + classList.size() + " classes to file: " + path.toAbsolutePath());
             }
+        } catch (IOException e) {
+            System.out.println(RED + "[ERROR] \t " + RESET + "Unable to write to file: " + e.getMessage());
+        }
+    }
+
+    // ToDo : Write proxy config (DONE)
+    public static void writeProxyConfig (List<String> proxyList, Path path) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayNode proxyConfig = objectMapper.createArrayNode();
+        try {
+            if (proxyList.isEmpty()) {
+                System.out.println(YELLOW + "[WARN] \t " + RESET + "No classes found to write to file");
+            } else {
+                for (String proxyName : proxyList) {
+                    String fullClassName = proxyName.replace("import", "").replace(";", "").trim();
+                    ObjectNode proxyNode = objectMapper.createObjectNode();
+                    proxyNode.put("interfaces", fullClassName);
+                    proxyConfig.add(proxyNode);
+                }
+                objectMapper.writeValue(new File(path.toString()), proxyConfig);
+                System.out.println(BLUE + "[INFO] \t " + RESET + "Wrote " + proxyList.size() + " classes to file: " + path.toAbsolutePath());
+            }
+        } catch (IOException e) {
+            System.out.println(RED + "[ERROR] \t " + RESET + "Unable to write to file: " + e.getMessage());
+        }
+    }
+
+    // ToDo : Path validation ()
+    public static void isAllPathsValid () {
+        System.out.println(BLUE + "[RUNNING] \t " + RESET + "Starting path validation");
+        try {
+            Map<String, Path> pathMap = Map.of(
+                    "projectPath", projectPath,
+                    "repositoryPath", repositoryPath,
+                    "outputPath", outputPath
+            );
+            Map<String, Path> reflectionFilePathMap = Map.of(
+                    "projectReflectionPath", projectReflectionPath,
+                    "importReflectionPath", importReflectionPath,
+                    "pomReflectionPath", pomReflectionPath,
+                    "dependenciesReflectionPath", dependenciesReflectionPath,
+                    "fatJarReflectionPath", fatJarReflectionPath
+            );
+
+            Map<String, Path> proxyFilePathMap = Map.of(
+                    "projectProxyPath", projectProxyPath,
+                    "importProxyPath", importProxyPath,
+                    "pomProxyPath", pomProxyPath,
+                    "dependenciesProxyPath", dependenciesProxyPath,
+                    "fatJarProxyPath", fatJarProxyPath
+            );
+
+            Map<String, Path> outputFilePathMap = Map.of(
+                    "reflectConfigFile", reflectConfigFile,
+                    "proxyConfigFile", proxyConfigFile
+            );
+
+            List<String> invalidPaths = new ArrayList<>();
+            List<String> validPaths = new ArrayList<>();
+            for (Map.Entry<String, Path> entry : pathMap.entrySet()){
+                Path path = entry.getValue();
+                if (!Files.exists(path) || !Files.isDirectory(path) || path.toString().isEmpty()){
+                    invalidPaths.add(entry.getKey() + " -> " + path);
+                } else {
+                    validPaths.add(entry.getKey() + " -> " + path);
+                }
+            }
+
+            List<String> invalidReflectionPaths = new ArrayList<>();
+            List<String> validReflectionPaths = new ArrayList<>();
+            for (Map.Entry<String, Path> entry : reflectionFilePathMap.entrySet()){
+                File file = new File(entry.getValue().toString());
+                Path path = entry.getValue();
+                if (!file.exists()){
+                    invalidReflectionPaths.add(entry.getKey() + " -> " + path);
+                } else {
+                    validReflectionPaths.add(entry.getKey() + " -> " + path);
+                }
+            }
+
+            List<String> invalidProxyPaths = new ArrayList<>();
+            List<String> validProxyPaths = new ArrayList<>();
+            for (Map.Entry<String, Path> entry : proxyFilePathMap.entrySet()){
+                File file = new File(entry.getValue().toString());
+                Path path = entry.getValue();
+                if (!file.exists()){
+                    invalidProxyPaths.add(entry.getKey() + " -> " + path);
+                } else {
+                    validProxyPaths.add(entry.getKey() + " -> " + path);
+                }
+            }
+
+            List<String> invalidOutputPaths = new ArrayList<>();
+            List<String> validOutputPaths = new ArrayList<>();
+            for (Map.Entry<String, Path> entry : outputFilePathMap.entrySet()){
+                File file = new File(entry.getValue().toString());
+                Path path = entry.getValue();
+                if (!file.exists()){
+                    invalidOutputPaths.add(entry.getKey() + " -> " + path);
+                } else {
+                    validOutputPaths.add(entry.getKey() + " -> " + path);
+                }
+            }
+            System.out.println(CYAN + "[INFO] \t\t "+ "Registered paths:" + RESET );
+            for (String path : validPaths){System.out.println(
+                    CYAN + "[INFO] \t\t " + RESET + GREEN + "Valid path: " + RESET + path
+            );}
+            for (String path : invalidPaths){System.out.println(
+                    RED + "[ERROR] \t " + RESET + RED + "Invalid path: " + RESET + path
+            );}
+
+            System.out.println(CYAN + "[INFO] \t\t "+ "Registered reflection paths:" + RESET );
+            for (String path : validReflectionPaths){System.out.println(
+                    CYAN + "[INFO] \t\t " + RESET + GREEN + "Valid path: " + RESET + path
+            );}
+            for (String path : invalidReflectionPaths){System.out.println(
+                    RED + "[ERROR] \t " + RESET + RED + "Invalid path: " + RESET + path
+            );}
+
+            System.out.println(CYAN + "[INFO] \t\t "+ "Registered proxy paths:" + RESET );
+            for (String path : validProxyPaths){System.out.println(
+                    CYAN + "[INFO] \t\t " + RESET + GREEN + "Valid path: " + RESET + path
+            );}
+            for (String path : invalidProxyPaths){System.out.println(
+                    RED + "[ERROR] \t " + RESET + RED + "Invalid path: " + RESET + path
+            );}
+
+            System.out.println(CYAN + "[INFO] \t\t " + "Registered output paths:" + RESET);
+            for (String path : validOutputPaths){System.out.println(
+                    CYAN + "[INFO] \t\t " + RESET + GREEN + "Valid path: " + RESET + path
+            );}
+            for (String path : invalidOutputPaths){System.out.println(
+                    RED + "[ERROR] \t " + RESET + RED + "Invalid path: " + RESET + path
+            );}
+
+
+            if (invalidPaths.size() > 0 ||
+                    invalidReflectionPaths.size() > 0 ||
+                    invalidProxyPaths.size() > 0 ||
+                    invalidOutputPaths.size() > 0
+            ) {
+//                System.exit(0);
+            }
+
+        } catch (Exception e){
+            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred during path validation: " + e.getMessage());
+        }
+        System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Path validation complete\n");
+    }
+
+    // ToDo : Run mvn dependency:run to list all project's dependencies ()
+    public static void listAllDependencies () {
+        System.out.println(BLUE + "[RUNNING] \t " + RESET + "Listing all project dependencies");
+        try {
+
+        } catch (Exception e){
+            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred while listing project dependencies: " + e.getMessage());
+        }
+        System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Finished listing all project dependencies\n");
+    }
+
+    // ToDo : Run mvn clean package -DskipTests to generate fat jar ()
+    public static void buildFatJar () {
+        System.out.println(BLUE + "[RUNNING] \t " + RESET + "Building project fat jar");
+        try {
+
+        } catch (Exception e){
+            System.out.println(RED + "[ERROR] \t " + RESET + "An error occurred while building project fat jar: " + e.getMessage());
+        }
+        System.out.println(GREEN + "[COMPLETE] \t " + RESET + "Finished Building project fat jar\n");
+    }
+
+    // ToDo : Just troll the users
+    public static void printLogo() {
+        try {
+            ClassPathResource resource = new ClassPathResource("banner.txt");
+            List<String> lines = Files.readAllLines(resource.getFile().toPath());
+            lines.forEach(line -> {
+                String line1 = substringSafe(line, 0, 32);
+                String line2 = substringSafe(line, 34, 64);
+                String line3 = substringSafe(line, 65, 90);
+                System.out.println(
+                        RED + line1 + RESET +
+                        BLUE + line2 + RESET +
+                        GREEN + line3 + RESET);
+            });
             System.out.println("Metadata Generator Version : " + version);
             System.out.println("Project Name : " + projectName + "\n");
         } catch (IOException e) {
             System.out.println(RED + "[ERROR] \t " + RESET + "Unable to load logo: " + e.getMessage());
         }
+    }
+    private static String substringSafe(String str, int start, int end) {
+        if (start >= str.length()) return "";
+        return str.substring(start, Math.min(end, str.length()));
     }
 }
